8418dc02188cd1a20202ee2cd34f11914c20d857
grleung <gabrielle.leung@colostate.edu>
2024-08-23 14:16:10 -0600

    changes to switch to dask

--- shared_functions.py
+++ shared_functions.py
@@ -3,6 +3,17 @@ import numpy as np
 import xarray as xr
 from jug import TaskGenerator
 import tobac
+import dask
+import os
+
+# physical constants
+g = 9.8065
+eps = 0.622
+cp = 1004
+rd = 287
+p00 = 100000
+rgas = 287
+lv = 2.5e6
 
 rams_dims_lite = {
     "phony_dim_0": "p",
@@ -22,33 +33,63 @@ rams_dims_anal = {
 
 
 def get_rams_output(
-    p, variables, dims=rams_dims_lite, latlon=True, coords=True
+    path, variables, latbounds=None, dims=rams_dims_lite, latlon=True, coords=True
 ):
-    # read in a RAMS output file, returns an xarray dataset
+    time = pd.to_datetime(path.split("/")[-1][4:-6])
+    grid = path.split("/")[-1][-5:-3]
 
     if latlon or coords:
-        variables = variables + ["GLAT", "GLON"]
+        drop_var = [v for v in all_var if v not in variables + ["GLAT", "GLON"]]
+    else:
+        drop_var = [v for v in all_var if v not in variables]
 
-    ds = xr.open_dataset(p, phony_dims="access", engine="h5netcdf")[variables]
-    ds = ds.rename_dims(dict([(d, dims.get(d)) for d in ds.dims]))
+    ds = xr.open_dataset(
+        path,
+        phony_dims="access",
+        engine="h5netcdf",
+        chunks="auto",
+        drop_variables=drop_var,
+    )
+
+    ds = rename_dims(ds, dims)
+
+    if grid != "g3":
+        ds = subset_data(ds, latbounds)
 
     if coords:
-        ds = ds.assign_coords(
-            {
-                "X": ds.X,
-                "Y": ds.Y,
-                "ztn": ds.ztn,
-                "lat": (["Y", "X"], np.array(ds.GLAT)),
-                "lon": (["Y", "X"], np.array(ds.GLON)),
-            }
-        )
+        ds = assign_coords(ds)
+
+    if len(variables) == 1:
+        ds = ds[variables[0]]
 
     return ds
 
 
+def rename_dims(ds, dims=rams_dims_lite):
+    return ds.rename_dims(dict([(d, dims.get(d)) for d in ds.dims]))
+
+
+def assign_coords(ds):
+    if "ztn" in ds.dims:
+        c = {
+            "X": ds.X,
+            "Y": ds.Y,
+            "ztn": ds.ztn,
+            "lat": (["Y", "X"], np.array(ds.GLAT)),
+            "lon": (["Y", "X"], np.array(ds.GLON)),
+        }
+    else:
+        c = {
+            "X": ds.X,
+            "Y": ds.Y,
+            "lat": (["Y", "X"], np.array(ds.GLAT)),
+            "lon": (["Y", "X"], np.array(ds.GLON)),
+        }
+    return ds.assign_coords(c)
+
+
 def get_xy_spacing(grid):
     # returns the grid spacing in m
-
     if grid == "g1":
         return 1600
     elif grid == "g2":
@@ -60,24 +101,95 @@ def get_xy_spacing(grid):
 def subset_data(ds, latbounds):
     # takes an xarray dataset ds from RAMS output and selects only
     # the values within the defined lat/lon boundaries
-    ds = ds.where(
-        (ds.GLAT >= latbounds[0])
-        & (ds.GLAT <= latbounds[1])
-        & (ds.GLON >= latbounds[2])
-        & (ds.GLON <= latbounds[3]),
-        drop=True,
+    ds = (
+        ds.where(
+            (ds.GLAT >= latbounds[0])
+            & (ds.GLAT <= latbounds[1])
+            & (ds.GLON >= latbounds[2])
+            & (ds.GLON <= latbounds[3]),
+        )
+        .dropna(dim="X", how="all")
+        .dropna(dim="Y", how="all")
     )
 
     return ds
 
 
-@TaskGenerator
 def combine_tobac_list(features_list):
     # takes a list of tobac output dataframes and combines them into one dataframe
     return tobac.utils.combine_feature_dataframes(features_list)
 
 
-@TaskGenerator
-def save_df(df, savePath):
-    # save dataframe as pickle in savePath
-    df.to_pickle(savePath)
+def save_files(out, savePath):
+    out["time"] = pd.to_datetime(out["timestr"])
+    # save dataframe as a parquet in savePath
+    out.to_parquet(savePath, engine="pyarrow")
+
+
+def compute_cond(ds):
+    ds = ds.assign(PRES=p00 * (ds.PI / cp) ** (cp / rd))
+    ds = ds.assign(TEMP=ds.THETA * (ds.PI / cp))
+    ds = ds.assign(DENS=ds.PRES / (rd * ds.TEMP * (1 + (0.61 * ds.RV))))
+
+    ds = ds.assign(COND=(ds.RCP + ds.RSP + ds.RPP) * ds.DENS)
+
+    ds = ds["COND"]
+    return ds
+
+
+all_var = [
+    "CAN_TEMP",
+    "CAP",
+    "CCP",
+    "CDP",
+    "CGP",
+    "CHP",
+    "CIFNP",
+    "CN1NP",
+    "CPP",
+    "CRP",
+    "CSP",
+    "FTHRD",
+    "GLAT",
+    "GLON",
+    "LATHEATFRZ",
+    "LATHEATVAP",
+    "PATCH_ROUGH",
+    "PCPRA",
+    "PCPRD",
+    "PCPRG",
+    "PCPRH",
+    "PCPRP",
+    "PCPRR",
+    "PCPRS",
+    "PI",
+    "PP",
+    "Q6",
+    "Q7",
+    "RAP",
+    "RCP",
+    "RDP",
+    "REGEN_AERO1_NP",
+    "RGP",
+    "RHP",
+    "RLONTOP",
+    "RPP",
+    "RRP",
+    "RSP",
+    "RTP",
+    "RV",
+    "SFLUX_R",
+    "SFLUX_T",
+    "SOIL_ENERGY",
+    "SOIL_WATER",
+    "THETA",
+    "TOPT",
+    "TSTAR",
+    "UP",
+    "USTAR",
+    "VEG_TEMP",
+    "VP",
+    "WP",
+    "WP_ADVDIF",
+    "WP_BUOY_COND",
+]
--- w_feature_identification.py
+++ w_feature_identification.py
@@ -4,60 +4,51 @@
 # inputs: list of model runs, RAMS model output
 # output: tobac output dataframe of features
 
-# To run this script, in command line enter "jug execute w_feature_identification.py &"
-# repeated n times (where n is the number of processes you want to run).
-# This script will process each timestep separately then stitch them together.
+# This script is parallelized with dask distributed. To run from command line
+# run "dask scheduler &" then "dask workers tcp::/scheduler:port --nworkers NW --nthreads NT --memory-limit "MEM GiB"
+# I usually use NW = 30, NT = 1, MEM = 30 GiB, but haven't really experimented with this.
+# then run "python w_feature_identification.py"
+# Make sure that the client below is the same as the client address of your scheduler.
+# Pretty sure this is the default and should work, but I may be wrong.
 
 # For questions, contact Bee (gabrielle.leung@colostate.edu)
 
 # Import some shared libraries
 import os
-from jug import TaskGenerator
+import dask.distributed as dd
+import dask
 import numpy as np
 import pandas as pd
 import xarray as xr
 import tobac
+
+client = dd.Client("tcp://129.82.20.48:8786")
+client.upload_file("shared_functions.py")
+
 from shared_functions import (
     get_rams_output,
     get_xy_spacing,
     subset_data,
     combine_tobac_list,
-    save_df,
+    rams_dims_lite,
+    all_var,
+    save_files,
 )
-
 
 # Define the paths to INCUS data and where to save output
 ver = "V1"  # version of INCUS simulation dataset
 modelPath = f"/monsoon/MODEL/LES_MODEL_DATA/{ver}/"
 outPath = f"/monsoon/MODEL/LES_MODEL_DATA/Tracking/{ver}/"
-runs = ["DRC1.1-R-V1"]  # which model runs to process
 
-"""
-# This will make a list of runs that has all variabels in modelPath where 30-s output exists
-runs = [
-    r
-    for r in os.listdir(modelPath)
-    if (os.path.exists(f"{modelPath}/{r}/G3/out_30s"))
-]
+runs = ['DRC1.1-R-V1','ARG1.2-R-V1']# which model runs to process
+grids = [
+    "g1","g2",'g3'
+]  
 
-runs = [
-    r
-    for r in sorted(runs)
-    if (
-        (
-            len(
-                os.listdir(
-                    f"{modelPath}/{r}/G3/out_30s"
-                )
-            )
-            > 0
-        )
-        & ("old" not in r)
-    )
-]
-
-runs = [r for r in runs if ("-R-" in r)] # subset only base aerosol
-"""
+# separately I created a pkl file that contains the min/max lat/lon for each of the simulations
+# having this as separate dataframe saves on some computational cost from re-calculating this
+# in every script
+outbounds = pd.read_pickle(f"/tempest/gleung/incustrack/bounds.pkl")
 
 # tobac feature identification parameters
 # see tobac documentation for more detailed description
@@ -72,42 +63,7 @@ params["target"] = "maximum"
 # threshold in m/s is (1, 2, 4, 6, ..., 50)
 params["threshold"] = np.append([1.0], np.arange(2.0, 52.0, 2.0))
 
-
-@TaskGenerator
-def get_w_features(dataPath, p, grid, latbounds):
-    # parse the file path to get datetime since format is always the same in RAMS
-    time = pd.to_datetime(p[4:])
-    p = f"{dataPath}/{p}-{grid}.h5"
-
-    # assign dxy (horizontal grid spacing) based on grid
-    dxy = get_xy_spacing(grid)
-
-    # read in the RAMS data and assign coordinates in format tobac expects
-    ds = get_rams_output(p, ["WP"], latlon=True, coords=True)
-    ds = ds.expand_dims(dim="time")
-    ds["time"] = [time]
-
-    # subset data based on lat/lon bounds
-    # this is to make sure that we're using the same spatial location for all the grids
-    ds = subset_data(ds, latbounds)
-
-    # right now, tobac doesn't have xarray support, so convert to an iris cube
-    iris_df = ds["WP"].to_iris()
-
-    # call tobac feature detection
-    Features = tobac.feature_detection_multithreshold(
-        iris_df, dxy, vertical_coord="ztn", **params
-    )
-
-    return Features
-
-
-# separately I created a pkl file that contains the min/max lat/lon for each of the simulations
-# having this as separate dataframe saves on some computational cost from re-calculating this
-# in every script
-outbounds = pd.read_pickle(f"/tempest/gleung/incustrack/bounds.pkl")
-
-# actual loop
+# loop through each of the runs
 for run in runs:
     dataPath = f"{modelPath}/{run}/G3/out_30s/"
 
@@ -120,9 +76,54 @@ for run in runs:
 
     latbounds = outbounds.loc[run].values
 
-    for grid in ["g1", "g2", "g3"]:
-        # define path to save variable in
-        savePath = f"{outPath}/{run}/{grid}/w_features.pkl"
+    for grid in grids:
+        savePath = f"{outPath}/{run}/{grid}/w_features.pq"
+
+        if grid == "g3":
+            batch_size = 5
+        else:
+            batch_size = 20
+
+        dxy = get_xy_spacing(grid)
+
+        ds = client.map(
+            get_rams_output,
+            [f"{dataPath}/{p}-{grid}.h5" for p in paths],
+            variables=["WP"],
+            
+            latbounds=latbounds,
+            latlon=True,
+            coords=True,
+            batch_size=batch_size,
+        )
+
+        ds = client.map(
+            xr.DataArray.expand_dims,
+            ds,
+            [{"time": [pd.to_datetime(p.split("/")[-1][4:])]} for p in paths],
+            batch_size=batch_size,
+        )
+
+        ds = client.map(
+            xr.DataArray.to_iris,
+            ds,
+            batch_size=batch_size,
+        )
+
+        feats = client.map(
+            tobac.feature_detection_multithreshold,
+            ds,
+            dxy=dxy,
+            vertical_coord="ztn",
+            **params,
+            batch_size=batch_size,
+        )
+
+        all_features = client.gather(feats)
+
+        # once loop is finished, concatenate all the figures
+        # then save it to a parquet file
+        all_features = combine_tobac_list(all_features)
 
         # just make sure all directories exist
         if not os.path.isdir(f"{outPath}/{run}"):
@@ -131,12 +132,4 @@ for run in runs:
         if not os.path.isdir(f"{outPath}/{run}/{grid}"):
             os.mkdir(f"{outPath}/{run}/{grid}")
 
-        # loop over timesteps and append each output dataframe to list
-        all_features = list()
-        for p in paths:
-            all_features.append(get_w_features(dataPath, p, grid, latbounds))
-
-        # once loop is finished, concatenate all the figures and
-        # save dataframe as a pickle in savePath
-        combined_df = combine_tobac_list(all_features)
-        save_df(combined_df, savePath)
+        save_files(all_features, savePath)
